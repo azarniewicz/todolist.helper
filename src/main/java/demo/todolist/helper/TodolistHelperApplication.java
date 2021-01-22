04cb8ef5e327560ae0c6662937a1d78d542c1176
azarniewicz <a.zarniewicz@gmail.com>
2021-01-22 17:27:47 +0100

    init

---
+++ src/main/java/demo/todolist/helper/TodolistHelperApplication.java
@@ -0,0 +1,13 @@
+package demo.todolist.helper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodolistHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodolistHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demo/todolist/helper/TodolistHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.todolist.helper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
